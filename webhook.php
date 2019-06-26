581e3594a506979e2f51fd6c4d150c73e40b74de
Tip8rn <tip8rn@gmail.com>
2019-06-26 09:41:42 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -215,16 +215,17 @@ if(!is_null($events)){
                             )
                         );
                     break;
+             case "สถานการณ์บุหรี่":
+                    $textReplyMessage = "https://cloud.ddc.moph.go.th/remote.php/webdav/Documents/%E0%B8%AA.%E0%B8%A2%E0%B8%B2%E0%B8%AA%E0%B8%B9%E0%B8%9A/%E0%B8%A3%E0%B8%B2%E0%B8%A2%E0%B8%87%E0%B8%B2%E0%B8%99%E0%B8%AA%E0%B8%96%E0%B8%B2%E0%B8%99%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B8%93%E0%B9%8C%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B8%9A%E0%B8%A3%E0%B8%B4%E0%B9%82%E0%B8%A0%E0%B8%84%E0%B8%A2%E0%B8%B2%E0%B8%AA%E0%B8%B9%E0%B8%9A%20%E0%B8%9B%E0%B8%B5%202562.pdf";
+                    $replyData = new TextMessageBuilder($textReplyMessage);
+                    break;
                 default:
                     $textReplyMessage = " คุณไม่ได้พิมพ์ ค่า ตามที่กำหนด";
                     $replyData = new TextMessageBuilder($textReplyMessage);
                     break;
             }
             break;
-            case "สถานการณ์บุหรี่":
-                    $textReplyMessage = "https://cloud.ddc.moph.go.th/remote.php/webdav/Documents/%E0%B8%AA.%E0%B8%A2%E0%B8%B2%E0%B8%AA%E0%B8%B9%E0%B8%9A/%E0%B8%A3%E0%B8%B2%E0%B8%A2%E0%B8%87%E0%B8%B2%E0%B8%99%E0%B8%AA%E0%B8%96%E0%B8%B2%E0%B8%99%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B8%93%E0%B9%8C%E0%B8%81%E0%B8%B2%E0%B8%A3%E0%B8%9A%E0%B8%A3%E0%B8%B4%E0%B9%82%E0%B8%A0%E0%B8%84%E0%B8%A2%E0%B8%B2%E0%B8%AA%E0%B8%B9%E0%B8%9A%20%E0%B8%9B%E0%B8%B5%202562.pdf";
-                    $replyData = new TextMessageBuilder($textReplyMessage);
-                    break;
+
         default:
             $textReplyMessage = json_encode($events);
             $replyData = new TextMessageBuilder($textReplyMessage);
