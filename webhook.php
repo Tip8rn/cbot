22feaf040bd9fc95a0fc15269f9a430924cc082e
Tip8rn <tip8rn@gmail.com>
2019-09-02 14:13:53 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -137,6 +137,11 @@ if(!is_null($events)){
                         )
                     );
                     break;
+                case "บันได":
+                    $picFullSize = 'https://cloud.ddc.moph.go.th/index.php/s/LtaNfHrpPKiTsHa';
+                    $picThumbnail = 'https://cloud.ddc.moph.go.th/index.php/apps/files_sharing/ajax/publicpreview.php?x=1366&y=226&a=true&file=staircase10.png&t=LtaNfHrpPKiTsHa&scalingup=0';
+                    $replyData = new ImageMessageBuilder($picFullSize,$picThumbnail);
+                    break;
                     case "สถานการณ์โรค":
                         // กำหนด action 4 ปุ่ม 4 ประเภท
                         $actionBuilder1 = array(
@@ -229,12 +234,6 @@ if(!is_null($events)){
                 default:
                     $textReplyMessage = " คุณไม่ได้พิมพ์ ค่า ตามที่กำหนด";
                     $replyData = new TextMessageBuilder($textReplyMessage);
-                    break;
-
-             case "บันได":
-                    $picFullSize = 'https://cloud.ddc.moph.go.th/index.php/s/LtaNfHrpPKiTsHa';
-                    $picThumbnail = 'https://cloud.ddc.moph.go.th/index.php/apps/files_sharing/ajax/publicpreview.php?x=1366&y=226&a=true&file=staircase10.png&t=LtaNfHrpPKiTsHa&scalingup=0';
-                    $replyData = new ImageMessageBuilder($picFullSize,$picThumbnail);
                     break;
             }
         default:
